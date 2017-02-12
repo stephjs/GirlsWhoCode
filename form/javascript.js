26644b6e178b5652248a97f53b74f1e11cfd0537
stephorpilla <stephanieorpilla@yahoo.com>
2017-02-12 12:22:58 -0800

    coding forms

---
+++ form/javascript.js
@@ -0,0 +1,6 @@
+function ValidationEvent() {
+	var firstname = document.getElementById("firstname").value;
+	var lastname = document.getElementById("lastname").value;
+
+	alert("Hello "+firstname+" "+lastname);
+}
